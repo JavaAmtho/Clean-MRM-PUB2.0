39e6e1894e5a9f36b73ce5329bd3501b9cbdc93f
amar <amar@inbetween.co.in>
2013-12-11 15:43:19 +0530

    Mocks removed from the GetMarkers interactor.

--- delivery/src/main/webapp/WEB-INF/pages/mrm/graphics/screens/home/interactors/GetMarkers.js
+++ delivery/src/main/webapp/WEB-INF/pages/mrm/graphics/screens/home/interactors/GetMarkers.js
@@ -3,25 +3,6 @@ function GetMarkers(){
 }
 
 GetMarkers.getAllMarkers = function(callBack){
-    var data = {
-        "Category 1": [
-            {
-                "markerName": "Category1Marker1"
-            },
-            {
-                "markerName": "Category1Marker2"
-            }
-        ],
-        "Category 2": [
-            {
-                "markerName": "Category2Marker1"
-            },
-            {
-                "markerName": "Category2Marker2"
-            }
-        ]
-    }
-    callBack(data);
-     //Router.loadPhpGetRequest("getAllMarkers",false,callBack)
+     Router.loadPhpGetRequest("getAllMarkers",false,callBack)
 }
 
